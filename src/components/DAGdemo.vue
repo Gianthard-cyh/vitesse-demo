0881b2fc8bb193da1bd9844730ff742580dccb57
Gianthard-cyh <2953427626@qq.com>
2022-09-21 13:05:43 +0800

    chore: remove X6 comments

--- src/components/DAGdemo.vue
+++ src/components/DAGdemo.vue
@@ -7,27 +7,27 @@ const graphData = {
   // 节点
   nodes: [
     {
-      id: 'node1', // String，可选，节点的唯一标识
-      x: 40, // Number，必选，节点位置的 x 值
-      y: 40, // Number，必选，节点位置的 y 值
-      width: 80, // Number，可选，节点大小的 width 值
-      height: 40, // Number，可选，节点大小的 height 值
-      label: 'hello', // String，节点标签
+      id: 'node1',
+      x: 40,
+      y: 40,
+      width: 80,
+      height: 40,
+      label: 'hello',
     },
     {
-      id: 'node2', // String，节点的唯一标识
-      x: 160, // Number，必选，节点位置的 x 值
-      y: 180, // Number，必选，节点位置的 y 值
-      width: 80, // Number，可选，节点大小的 width 值
-      height: 40, // Number，可选，节点大小的 height 值
-      label: 'world', // String，节点标签
+      id: 'node2',
+      x: 160,
+      y: 180,
+      width: 80,
+      height: 40,
+      label: 'world',
     },
   ],
   // 边
   edges: [
     {
-      source: 'node1', // String，必须，起始节点 id
-      target: 'node2', // String，必须，目标节点 id
+      source: 'node1',
+      target: 'node2',
     },
   ],
 }
